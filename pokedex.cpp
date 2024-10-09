2ba900af7e74e91f8911cdc98393b7002f95db55
Anthony Enriquez <tenriquez1818@outlook.com>
2024-10-08 21:50:26 -0500

    first

---
+++ pokedex.cpp
@@ -0,0 +1,33 @@
+#include "pokedex.h"
+
+Pokedex::Pokedex(){
+
+}
+
+Pokedex::Pokedex(){
+
+}
+
+void Pokedex::save(string filename){
+
+}
+
+void Pokedex::add(Pokemon* p){
+
+}
+
+void Pokedex::remove(Pokemon* p){
+
+}
+
+Pokemon* Pokedex::lookup_by_name(string name){
+
+}
+
+Pokemon* Pokedex::lookup_by_Ndex(int ndex){
+
+}
+
+int Pokedex::size(){
+
+}
